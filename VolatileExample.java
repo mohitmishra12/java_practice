2276ab344186670b784f2c643be8fd5cd49933e6
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-04 23:23:16 +0530

    DemoThred

---
+++ VolatileExample.java
@@ -0,0 +1,34 @@
+
+class vole extends Thread{
+
+    volatile boolean running = true;
+
+    public void run(){
+        while (running){
+
+        }
+        System.out.println("stop....");
+    }
+
+    public void stope(){
+        running = false;
+
+    }
+
+    public void stopThread() {
+    }
+}
+
+
+
+public class volatileKeyword {
+    public static void main(String[] args) throws InterruptedException {
+        vole vole1 = new vole();
+        vole1.start();
+
+        Thread.sleep(1000);
+
+        vole1.stopThread();
+
+    }
+}
