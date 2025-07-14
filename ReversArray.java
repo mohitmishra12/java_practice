0e0be2d46caad6fd9ae2170def41d3ae4aa512bc
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-14 15:09:32 +0530

    new changes

--- ReversArray.java
+++ ReversArray.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ReversArray {
+
+public class ReversArray {
+    public static void main(String[] args) {
+
+        int arr [] = {23,4,5,678,99,78,66,75};
+        int start = 0 , end = arr.length-1;
+
+        while (start< end){
+            int temp =  arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+
+        System.out.println("Revers number : ");
+
+        for (int i : arr){
+            System.out.println( i + " ");
+        }
+
+    }
+
+
+
 }
+
+
+
--- ServletJava.java
+++ ServletJava.java
@@ -1,29 +1,29 @@
-import servlett.HttpServlet;
-
-class myservlet extends HttpServlet {
-
-    private Object req;
-    private ServletResponse res;
-
-    public void init(){
-        System.out.println("Servlet Initialized");
-
-    }
-    public <ServletRequest> void service(ServletRequest req, ServletResponse res){
-
-        this.req = req;
-        this.res = res;
-    }
-
-    public void destroy() {
-        System.out.println("Servlet Destroyed");
-    }
-
-}
-
-
-//Servelt practice
-
-public class ServletJava {
-
-}
+//import servlett.HttpServlet;
+//
+//class myservlet extends HttpServlet {
+//
+//    private Object req;
+//    private ServletResponse res;
+//
+//    public void init(){
+//        System.out.println("Servlet Initialized");
+//
+//    }
+//    public <ServletRequest> void service(ServletRequest req, ServletResponse res){
+//
+//        this.req = req;
+//        this.res = res;
+//    }
+//
+//    public void destroy() {
+//        System.out.println("Servlet Destroyed");
+//    }
+//
+//}
+//
+//
+////Servelt practice
+//
+//public class ServletJava {
+//
+//}
