f4498341de054f0c60da56098928728687028097
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-14 15:17:27 +0530

    new changes

---
+++ ArrayTraversal.java
@@ -0,0 +1,13 @@
+
+
+public class ArrayTraversal {
+    public static void main(String[] args) {
+
+        int arr [] = {12,32,4,45,66,78,45,34,75,45};
+        System.out.println("this is my ArrayTraversal List :");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + ": " + arr[i]);
+        }
+    }
+}
