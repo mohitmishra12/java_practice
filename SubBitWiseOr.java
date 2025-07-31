2cd77574bc60e1e79b9382646f41d6adf22ff48b
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-31 12:26:34 +0530

    new changes are Done

---
+++ SubBitWiseOr.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class SubBitWiseOr {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> s = new HashSet<>();
+        s.add(0);
+        Set<Integer> ans = new HashSet<>();
+        for (int x : arr) {
+            Set<Integer> t = new HashSet<>();
+            for (int y : s) {
+                t.add(x | y);
+            }
+            t.add(x);
+            s = t;
+            ans.addAll(s);
+        }
+        return ans.size();
+    }
+}
