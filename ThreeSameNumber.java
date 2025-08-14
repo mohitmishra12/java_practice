b4a816be0789a58404e9b88974d373ff7219d1af
mohitmishra12 <mohitmishra747041@gmail.com>
2025-08-14 11:10:22 +0530

    Three same number reapeted

---
+++ ThreeSameNumber.java
@@ -0,0 +1,11 @@
+class ThreeSameNumber {
+    public String largestGoodInteger(String num) {
+        for (int i = 9; i >= 0; i--) {
+            String ret = String.valueOf(i).repeat(3);
+            if (num.contains(ret)) {
+                return ret;
+            }
+        }
+        return "";
+    }
+}
