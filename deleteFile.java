118bee2a46f9371b091334ba00746367350b8643
mohitmishra12 <mohitmishra747041@gmail.com>
2025-08-20 11:28:25 +0530

    Delete file

--- deleteFile.java
+++ deleteFile.java
@@ -4,8 +4,10 @@ public class deleteFile {
     public static void main(String[] args) {
         File deleted = new File("new.txt");
         if (deleted.delete()){
+
             System.out.println("file deleted : " + deleted.getName());
         }else {
+
             System.out.println("failed to deleted file");
         }
     }
