743806f534247678fb21dc8740f3db772c0cf362
mohitmishra12 <mohitmishra747041@gmail.com>
2025-08-21 19:41:48 +0530

    array mejority

--- mejority_Arr.java
+++ mejority_Arr.java
@@ -6,6 +6,7 @@ class mejority_Arr {
         Deque<Character> stk = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
+
                 stk.push(c);
             } else if (stk.isEmpty() || !match(stk.pop(), c)) {
                 return false;
