f785a5084e3b025eb63161c840579c763eed682d
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-02 19:55:24 +0530

    new

--- Wirte_angle.java
+++ Wirte_angle.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
 class Wirte_angle {
-    public int numberOfPairs(int[][] points) {
-        Arrays.sort(points, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+    public int numberOfPairs(int[][] points) Arrays.sort(points, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
         int ans = 0;
         int n = points.length;
         final int inf = 1 << 30;
         for (int i = 0; i < n; ++i) {
             int y1 = points[i][1];
+
             int maxY = -inf;
             for (int j = i + 1; j < n; ++j) {
                 int y2 = points[j][1];
