fbc909b7694780c4fd2f09b5658764f7e57ddb33
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-05 19:56:21 +0530

    is same tree

---
+++ IssameTreee.java
@@ -0,0 +1,9 @@
+import javax.swing.tree.TreeNode;
+
+class IssameTreee {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == q) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
