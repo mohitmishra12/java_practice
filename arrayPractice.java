ccce04c048dc5d2592faed36d7844e7ee8bf5385
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-12 16:34:40 +0530

    array practice

---
+++ arrayPractice.java
@@ -0,0 +1,11 @@
+public class arrayPractice {
+    public static void main(String[] args) {
+
+        int [] arr = {34,45,67,34,23};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("printing number :" + i + " = " + arr[i]);
+
+        }
+    }
+}
