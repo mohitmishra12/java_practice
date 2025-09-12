bd027ffdee238280a61fed4066680a4b941b0f5c
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-12 16:15:03 +0530

    looping condition practic

---
+++ ifElseCondtion.java
@@ -0,0 +1,16 @@
+public class ifElseCondtion {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if(age<18){
+            System.out.println("are are looser");
+
+        } else if (age>=18 && age<60) {
+            System.out.println("You are an Adult");
+            
+        }else {
+            System.out.println("you are senior citizen ");
+        }
+    }
+}
