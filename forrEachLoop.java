e973de539ffd7c0e1086ff72cfb39d9b8229158a
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-12 16:30:43 +0530

    for loop do while and wbhile loop practice

---
+++ forrEachLoop.java
@@ -0,0 +1,10 @@
+public class forrEachLoop {
+    public static void main(String[] args) {
+
+        int [] num = {1,2,3,4,5};
+
+        for(int mynum : num){
+            System.out.println(mynum);
+        }
+    }
+}
