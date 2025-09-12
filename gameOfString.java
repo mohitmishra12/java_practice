9366535d64d80d0b5fbcb5d9c21066fae46d1f88
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-12 11:29:06 +0530

    Game Of String In LeedCode

---
+++ gameOfString.java
@@ -0,0 +1,10 @@
+class gameOfString {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            if ("aeiou".indexOf(s.charAt(i)) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
