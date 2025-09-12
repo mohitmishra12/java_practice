27529daa37ccb0c852fa71cf63ee5530ea878dd7
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-12 16:26:59 +0530

    for loop do while and wbhile loop practice

---
+++ forLoopPractice.java
@@ -0,0 +1,19 @@
+public class forLoopPractice {
+    public static void main(String[] args) {
+
+        int i = 6;
+        do {
+            System.out.println("Value: " + i);
+            i++;
+        } while (i <= 5);
+
+
+
+
+//        int i = 1;
+//        while (i <= 5) {
+//            System.out.println("Count: " + i);
+//            i++;
+//        }
+    }
+}
