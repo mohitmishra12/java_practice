35c9bb6f84a02f516bb40b76b1d14f1069284339
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-18 11:05:39 +0530

    design task manager

---
+++ designTaskManager.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
+class designTaskManager {
+    private final Map<Integer, int[]> d = new HashMap<>();
+    private final TreeSet<int[]> st = new TreeSet<>((a, b) -> {
+        if (a[0] == b[0]) {
+            return b[1] - a[1];
+        }
+        return b[0] - a[0];
+    });
+
+    public void TaskManager(List<List<Integer>> tasks) {
+        for (var task : tasks) {
+            add(task.get(0), task.get(1), task.get(2));
+        }
+    }
+
+    public void add(int userId, int taskId, int priority) {
+        d.put(taskId, new int[] {userId, priority});
+        st.add(new int[] {priority, taskId});
+    }
+
+    public void edit(int taskId, int newPriority) {
+        var e = d.get(taskId);
+        int userId = e[0], priority = e[1];
+        st.remove(new int[] {priority, taskId});
+        st.add(new int[] {newPriority, taskId});
+        d.put(taskId, new int[] {userId, newPriority});
+    }
+
+    public void rmv(int taskId) {
+        var e = d.remove(taskId);
+        int priority = e[1];
+        st.remove(new int[] {priority, taskId});
+    }
+
+    public int execTop() {
+        if (st.isEmpty()) {
+            return -1;
+        }
+        var e = st.pollFirst();
+        var t = d.remove(e[1]);
+        return t[0];
+    }
+}
+
