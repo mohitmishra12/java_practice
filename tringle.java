e984f7413d8256d78dc573b7c6e284340fce7270
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-25 14:53:06 +0530

    tringle

--- tringle.java
+++ tringle.java
@@ -3,15 +3,18 @@ import java.util.*;
 class tringle {
     int m;
     Integer[][] dp;
+
     public int minimumTotal(List<List<Integer>> tran) {
         m = tran.size();
         dp = new Integer[m][m];
 
         return helper(tran,0,0);
     }
+
     int helper(List<List<Integer>> tran,int row, int col){
         if(row==m){
             return 0;
+
         }
         if(dp[row][col]!=null){
             return dp[row][col];
