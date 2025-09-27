fd8c5ea30cafa5de4fa8cb1bbbcd247d3e0d9eb2
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-27 11:48:39 +0530

    interface in java

---
+++ FuncationalInterfaceNew.java
@@ -0,0 +1,20 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@FunctionalInterface
+interface MyNewInteface {
+    void hayMini(String name);
+
+    default void newoen() {
+        System.out.println("my new default merthod calling".toUpperCase().length());
+    }
+}
+public class FuncationalInterfaceNew {
+    public static void main(String[] args) {
+
+        MyNewInteface greet = (name -> System.out.println("name:" + name));
+        greet.hayMini("mohit");
+//        greet.toString().getBytes(StandardCharsets.UTF_8);
+        greet.newoen();
+    }
+}
