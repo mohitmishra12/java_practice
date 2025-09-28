f4803c0d10866a363831a3f71d14d7f9ee22e1b2
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-28 23:10:12 +0530

    thread in java

--- Lambda_practice.java
+++ Lambda_practice.java
@@ -1,6 +1,28 @@
+
+class mytred extends Thread{
+    public void newone(){
+
+        String myString = "Thread One are started";
+        System.out.println("new thread are started please wait after some time:" + myString);
+    }
+
+    int numone1 = 12;
+    public void after(){
+        System.out.println("after Thread are close :" + numone1);
+    }
+}
+
 public class Lambda_practice {
     public static void main(String[] args) {
-        System.out.println("practic next section");
+        Thread  mythered = new Thread();
+        mytred one = new mytred();
+        mytred after1 = new mytred();
+        after1.after();
+        one.newone();
+        one.start();
+        mythered.start();
+        mythered.isAlive();
+
 
     }
 }
