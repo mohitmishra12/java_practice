4af8c3a95b2af6713411b2592ad2626a7cb8cc99
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-29 19:17:33 +0530

    minmum Score in leed code

---
+++ ConstructorRef.java
@@ -0,0 +1,14 @@
+import java.util.function.Supplier;
+
+class Student{
+    void Student() {
+        System.out.println("Student object created!");
+    }
+}
+public class Studentt {
+    public static void main(String[] args) {
+        Supplier<Student> supplier = Student::new;
+        Student s = supplier.get();
+
+    }
+}
