a1ae23bd32059ae8058611b3ad38844e4ed9e9a1
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-13 09:46:08 +0530

    remove anagramas

---
+++ removeAgram.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class removeAgram {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> ans = new ArrayList<>();
+        String prev = "";
+        for (String w : words) {
+            char[] cs = w.toCharArray();
+            Arrays.sort(cs);
+            String t = String.valueOf(cs);
+            if (!t.equals(prev)) {
+                ans.add(w);
+            }
+            prev = t;
+        }
+        return ans;
+    }
+}
