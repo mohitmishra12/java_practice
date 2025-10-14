31e59f7b0824d0f7c5ae4e00bf17a6a8cd3f4896
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-14 23:07:51 +0530

    arraylist practice

---
+++ lambdaPracticess.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.*;
+
+
+public class lambdaPracticess {
+    public static void main(String[] args) {
+        List<Integer> newList = new ArrayList<>();
+        newList.add(12);
+        newList.add(54);
+        newList.add(78);
+        newList.add(74);
+
+        newList.remove(1).equals(12);
+        newList.forEach(n-> System.out.println(n));
+    }
+}
