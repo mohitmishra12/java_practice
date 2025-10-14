f7864c24930c7be2618ea2b9fe4d242244230e69
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-14 09:10:06 +0530

    incresing Sub ARRAY

--- removeAgrams.java
+++ removeAgrams.java
@@ -1,23 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*;
-
-class removeAgrams {
-    public List<String> removeAnagrams(String[] words) {
-        List<String> result = new ArrayList<>();
-        if (words == null || words.length == 0) {
-            return result;
-        }
-        int uniqueWordIdx = 0;
-        int currIdx = 1;
-        result.add(words[uniqueWordIdx]);
-        while (currIdx < words.length) {
-            if (!isAnagrams(words[currIdx], words[uniqueWordIdx])) {
-                uniqueWordIdx = currIdx;
-                result.add(words[uniqueWordIdx]);
-            }
-            currIdx++;
-        }
-        return result;
-    }
-}
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.*;
+//
+//class removeAgrams {
+//    public List<String> removeAnagrams(String[] words) {
+//        List<String> result = new ArrayList<>();
+//        if (words == null || words.length == 0) {
+//            return result;
+//        }
+//        int uniqueWordIdx = 0;
+//        int currIdx = 1;
+//        result.add(words[uniqueWordIdx]);
+//        while (currIdx < words.length) {
+//            if (!isAnagrams(words[currIdx], words[uniqueWordIdx])) {
+//                uniqueWordIdx = currIdx;
+//                result.add(words[uniqueWordIdx]);
+//            }
+//            currIdx++;
+//        }
+//        return result;
+//    }
+//}
