3963299ca3af1a560ce7e901df6333adb1ffdbcb
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-16 10:57:08 +0530

    find smallest integer in leed code

---
+++ findSmallestInteger.java
@@ -0,0 +1,13 @@
+class findSmallestInteger {
+    public int findSmallestInteger(int[] nums, int value) {
+        int[] cnt = new int[value];
+        for (int x : nums) {
+            ++cnt[(x % value + value) % value];
+        }
+        for (int i = 0;; ++i) {
+            if (cnt[i % value]-- == 0) {
+                return i;
+            }
+        }
+    }
+}
