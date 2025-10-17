6a3d255ac08c1ccd4898cc0f992312e61f07054a
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-17 09:02:22 +0530

    find smallest integer in leed code

--- lambdaPracticess.java
+++ lambdaPracticess.java
@@ -11,7 +11,8 @@ public class lambdaPracticess {
         newList.add(78);
         newList.add(74);
 
-//        newList.remove(1).equals(12);
+        newList.remove(1);
+
         newList.forEach(n-> System.out.println(n));
     }
 }
