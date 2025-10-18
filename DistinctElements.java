c052fb5a2541807ba1bee5c86f4cdd9dfc144bc6
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-18 12:48:02 +0530

    diistinct element

---
+++ DistinctElements.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class DistinctElements {
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int ans = 0, pre = Integer.MIN_VALUE;
+        for (int x : nums) {
+            int cur = Math.min(x + k, Math.max(x - k, pre + 1));
+            if (cur > pre) {
+                ++ans;
+                pre = cur;
+            }
+        }
+        return ans;
+    }
+}
