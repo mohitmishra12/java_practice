aa43c29f81aed4048abcf392d0d6cda4c060f4d0
mohitmishra12 <mohitmishra747041@gmail.com>
2025-10-31 10:01:12 +0530

    snakeNumber

--- snakeNumber.java
+++ snakeNumber.java
@@ -3,11 +3,13 @@ class snakeNumber {
         int[] ans = new int[2];
         int[] cnt = new int[100];
         int k = 0;
+
         for (int x : nums) {
             if (++cnt[x] == 2) {
                 ans[k++] = x;
             }
         }
+
         return ans;
     }
 }
