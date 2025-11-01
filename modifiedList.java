bb5a87b8bf6a348ad7f8355113885a2c8158d679
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-01 10:37:47 +0530

    modifiedListr

--- modifiedList.java
+++ modifiedList.java
@@ -1,21 +1,21 @@
-class modifiedList {
-    public ListNode modifiedList(int[] nums, ListNode head) {
-        int max = -1;
-        for(int num : nums ){
-            max = num > max ? num : max;
-        }
-        boolean[] freq = new boolean[max+1];
-        for(int num : nums) freq[num] = true;
-        ListNode temp = new ListNode();
-        ListNode current = temp;
-        while(head != null){
-            if( head.val >= freq.length || freq[head.val] == false){
-                current.next = head;
-                current = current.next;
-            }
-            head = head.next;
-        }
-        current.next = null;
-        return temp.next;
-    }
-}
+//class modifiedList {
+//    public ListNode modifiedList(int[] nums, ListNode head) {
+//        int max = -1;
+//        for(int num : nums ){
+//            max = num > max ? num : max;
+//        }
+//        boolean[] freq = new boolean[max+1];
+//        for(int num : nums) freq[num] = true;
+//        ListNode temp = new ListNode();
+//        ListNode current = temp;
+//        while(head != null){
+//            if( head.val >= freq.length || freq[head.val] == false){
+//                current.next = head;
+//                current = current.next;
+//            }
+//            head = head.next;
+//        }
+//        current.next = null;
+//        return temp.next;
+//    }
+//}
