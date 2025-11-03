7417cb3cd9b6c6eb658c10aadfc0d4211b57840d
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-03 15:34:45 +0530

    min cost

--- minCostt.java
+++ minCostt.java
@@ -3,9 +3,14 @@ class minCostt {
         int ans = 0;
         int maxNeededTime = neededTime[0];
 
+
+
         for (int i = 1; i < colors.length(); ++i)
             if (colors.charAt(i) == colors.charAt(i - 1)) {
+
                 ans += Math.min(maxNeededTime, neededTime[i]);
+
+
 
                 maxNeededTime = Math.max(maxNeededTime, neededTime[i]);
             } else {
