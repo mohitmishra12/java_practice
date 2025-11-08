8d5aeff22da7c86293fd0dc97ab7da92a20fc41f
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-08 20:45:45 +0530

    maxNewPower

---
+++ minBitOperation.java
@@ -0,0 +1,9 @@
+class minBitOperation {
+    public int minimumOneBitOperations(int n) {
+        int ans = 0;
+        for (; n > 0; n >>= 1) {
+            ans ^= n;
+        }
+        return ans;
+    }
+}
