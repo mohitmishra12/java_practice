436645ba76db085a53fbd29e26d985ee9c3edcaa
Mohit Mishra <mohit.mishra@lycadigital.com>
2025-11-10 19:57:14 +0530

    new changes

--- ArraySearch.java
+++ ArraySearch.java
@@ -1,28 +1,28 @@
-public class ArraySearch {
-    public static void main(String[] args) {
-        int arr [] = { 23 ,34, 56,78,45,34};
-
-
-        int key = 34;
-        boolean found = false;
-
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] == key){
-                System.out.println("Element found at index: " + i);
-                found = true;
-                break;
-            }
-
-            
-        }
-
-        if(!found){
-            System.out.println("not found");
-
-        }
-
-
-    }
-}
+//public class ArraySearch {
+//    public static void main(String[] args) {
+//        int arr [] = { 23 ,34, 56,78,45,34};
+//
+//
+//        int key = 34;
+//        boolean found = false;
+//
+//
+//        for (int i = 0; i < arr.length; i++) {
+//
+//            if (arr[i] == key){
+//                System.out.println("Element found at index: " + i);
+//                found = true;
+//                break;
+//            }
+//
+//
+//        }
+//
+//        if(!found){
+//            System.out.println("not found");
+//
+//        }
+//
+//
+//    }
+//}
