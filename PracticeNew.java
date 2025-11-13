9d60fd1352a75cb13220028cdb969c145b7f5642
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-13 11:39:17 +0530

    new one practice in java leedcode

--- PracticeNew.java
+++ PracticeNew.java
@@ -2,9 +2,11 @@ class PracticeNew {
     public int maxOperations(String s) {
         int ans = 0, cnt = 0;
         int n = s.length();
+
         for (int i = 0; i < n; ++i) {
             if (s.charAt(i) == '1') {
                 ++cnt;
+
             } else if (i > 0 && s.charAt(i - 1) == '1') {
                 ans += cnt;
             }
