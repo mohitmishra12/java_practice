56b8eb8877d2547ddf00efdbc19bd29a8bdfe773
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-14 18:01:38 +0530

    new one practice in java leedcode

--- itraterNew.java
+++ itraterNew.java
@@ -1,2 +1,23 @@
+
+import java.util.*;
+
 public class itraterNew {
+    static void main() {
+        List<String> it =  new ArrayList<>();
+        it.add("A");
+        it.add("B");
+        it.add("C");
+        it.add("D");
+
+        Iterator<String> itr = it.iterator();
+        while (itr.hasNext()) {
+            System.out.printf("%s ", itr.next());
+            if (itr.hasNext()) {
+                System.out.print(" ");
+
+            }
+
+        }
+        System.out.println();
+    }
 }
--- listItrattoer.java
+++ listItrattoer.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class listItrattoer {
+    static void main() {
+
+        List<String> names = new ArrayList<String>();
+        names.add("A");
+        names.add("B");
+        names.add("C");
+
+        ListIterator<String> newIter = names.listIterator();
+
+        System.out.println("Forward:");
+        while(newIter.hasNext()){
+            System.out.println(newIter.next());
+        }
+        System.out.println("Backward:");
+        while(newIter.hasPrevious()){
+            System.out.println(newIter.previous());
+        }
+    }
 }
