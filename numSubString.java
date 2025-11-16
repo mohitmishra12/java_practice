74fa2ed66e4fe3cc2ebbfdcde8af4aa95c5608b0
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-16 21:31:48 +0530

    new one practice in java leedcode

---
+++ numSubString.java
@@ -0,0 +1,17 @@
+class numSubString {
+    public int numSub(String s) {
+        int MOD = (int)Math.pow(10, 9) + 7;
+        int total_count = 0;
+        int current_count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '1') {
+                current_count += 1;
+            } else {
+                current_count = 0;
+            }
+            total_count = (total_count + current_count) % MOD;
+        }
+        return total_count;
+    }
+}
