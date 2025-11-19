55d5d830fdfe2ddef4074959920e33b045a08cd2
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-19 12:48:46 +0530

    new one practice in java leedcode

--- findFinal.java
+++ findFinal.java
@@ -7,10 +7,12 @@ class findFinal {
         Set<Integer> s = new HashSet<>();
         for (int num : nums) {
             s.add(num);
+
         }
         while (s.contains(original)) {
             original <<= 1;
         }
+
         return original;
     }
 }
