a0737a6ada13af57eb611dd68ef0330a2ec943fd
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-19 12:48:20 +0530

    new one practice in java leedcode

---
+++ findFinal.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class findFinal {
+
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> s = new HashSet<>();
+        for (int num : nums) {
+            s.add(num);
+        }
+        while (s.contains(original)) {
+            original <<= 1;
+        }
+        return original;
+    }
+}
