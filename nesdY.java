ae85aafe58eec010b2a0ed807719ff131a1a02b9
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-22 18:37:02 +0530

    new one

--- nesdY.java
+++ nesdY.java
@@ -3,6 +3,7 @@ class nesdY {
         int operations = 0;
         for (int num : nums) {
             int remainder = num % 3;
+
             if (remainder != 0) {
                 operations += Math.min(remainder, 3 - remainder);
             }
