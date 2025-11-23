ed5c1a20383a22cc6c04a361dab5c060e3b92149
mohitmishra12 <mohitmishra747041@gmail.com>
2025-11-23 12:23:11 +0530

    new one

--- nesdY.java
+++ nesdY.java
@@ -1,14 +1,14 @@
-class nesdY {
-    public int minimumOperations(int[] nums) {
-        int operations = 0;
-        for (int num : nums) {
-            int remainder = num % 3;
-
-            if (remainder != 0) {
-                operations += Math.min(remainder, 3 - remainder);
-            }
-        }
-
-        return operations;
-    }
-}
+//class nesdY {
+//    public int minimumOperations(int[] nums) {
+//        int operations = 0;
+//        for (int num : nums) {
+//            int remainder = num % 3;
+//
+//            if (remainder != 0) {
+//                operations += Math.min(remainder, 3 - remainder);
+//            }
+//        }
+//
+//        return operations;
+//    }
+//}
