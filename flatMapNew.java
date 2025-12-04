8f738d467c35b5aecf7a281f0d6813b427f23a95
mohitmishra12 <mohitmishra747041@gmail.com>
2025-12-04 19:59:09 +0530

    funcational programming

---
+++ flatMapNew.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class flatMapNew {
+    public static void main(String[] args) {
+        List<List<String>> lists = Arrays.asList(
+                Arrays.asList("a", "b"),
+                Arrays.asList("c","d")
+        );
+
+        List<String> finalList = lists.stream()
+                .flatMap(list -> list.stream())
+                .collect(Collectors.toList());
+        System.out.println(finalList);
+    }
+}
