e362d0316dbab72f7607e4e1759ddcabe37f35d8
mohitmishra12 <mohitmishra747041@gmail.com>
2025-12-04 17:51:55 +0530

    funcational programming

---
+++ funcationProgramming.java
@@ -0,0 +1,14 @@
+
+@FunctionalInterface
+interface myEntity{
+    int myentityData(int a, int b);
+}
+
+public class funcationProgramming {
+    myEntity myentity = (a,b)->a *b;
+    myEntity myentity1 = (a,b)->a + b;
+
+     System.out.println("Add: " + myentity.calculate(5, 3));
+     System.out.println("multi: " + myentity.calculate(5, 3));
+
+}
