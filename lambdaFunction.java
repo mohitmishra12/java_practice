7d2d2added4860233c0d0da4094669a6aa4b21a0
mohitmishra12 <mohitmishra747041@gmail.com>
2025-12-29 12:16:57 +0530

    new Stream commint

---
+++ lambdaFunction.java
@@ -0,0 +1,14 @@
+
+interface myynew{
+    void objectt();
+}
+
+
+public class lambdaFunction {
+    public static void main(String[] args) {
+
+        myynew ss = () -> System.out.println("lambda function calling");
+        ss.objectt();
+
+    }
+}
